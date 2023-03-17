4e217fc07a2cbf4bfab644ea74318a2c7f185ccd
Josemilson Junior <junior.tn1@gmail.com>
2023-03-17 19:08:30 -0300

    Feat: implementacao de metodos BootCamp e Dev

--- src/main/java/br/com/jfs_junior/dominio/Dev.java
+++ src/main/java/br/com/jfs_junior/dominio/Dev.java
@@ -1,20 +1,33 @@
 package br.com.jfs_junior.dominio;
 
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class Dev {
     private String nome;
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudoConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp(Bootcamp bootcamp){}
+    public void inscreverBootcamp(Bootcamp bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
 
-    public void progredir(){}
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()) {
+            this.conteudoConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    }
 
-    public void calcularTotalXp() {}
+    public double calcularTotalXp() {
+        return this.conteudoConcluidos
+                .stream()
+                .mapToDouble(conteudo -> conteudo.calcularXP())
+                .sum();
+    }
 
     public String getNome() {
         return nome;
